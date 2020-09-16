218fc4318e563553956c065f8ac96604f53fc713
guertins <32344784+guertins@users.noreply.github.com>
2020-09-15 18:31:47 -0700

    Home and Galaxies screen

---
+++ DigiScriptor/DigiScriptor/HomeScreen.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace DigiScriptor
+{
+    partial class HomeScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeScreen));
+            this.btnLights = new System.Windows.Forms.Button();
+            this.lblDigi = new System.Windows.Forms.Label();
+            this.btnMoons = new System.Windows.Forms.Button();
+            this.btnSky = new System.Windows.Forms.Button();
+            this.btnGalaxies = new System.Windows.Forms.Button();
+            this.btnEarth = new System.Windows.Forms.Button();
+            this.btnPlanets = new System.Windows.Forms.Button();
+            this.btnStars = new System.Windows.Forms.Button();
+            this.btnNebulae = new System.Windows.Forms.Button();
+            this.txtboxCart = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblCart = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLights
+            // 
+            this.btnLights.BackColor = System.Drawing.Color.DimGray;
+            this.btnLights.Font = new System.Drawing.Font("Bernard MT Condensed", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLights.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLights.Location = new System.Drawing.Point(132, 154);
+            this.btnLights.Name = "btnLights";
+            this.btnLights.Size = new System.Drawing.Size(197, 87);
+            this.btnLights.TabIndex = 0;
+            this.btnLights.Text = "Dome Lights";
+            this.btnLights.UseVisualStyleBackColor = false;
+            this.btnLights.Click += new System.EventHandler(this.btnLights_Click);
+            // 
+            // lblDigi
+            // 
+            this.lblDigi.AutoSize = true;
+            this.lblDigi.Font = new System.Drawing.Font("Bernard MT Condensed", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDigi.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lblDigi.Location = new System.Drawing.Point(35, 40);
+            this.lblDigi.Name = "lblDigi";
+            this.lblDigi.Size = new System.Drawing.Size(283, 67);
+            this.lblDigi.TabIndex = 1;
+            this.lblDigi.Text = "DigiScriptor";
+            // 
+            // btnMoons
+            // 
+            this.btnMoons.BackColor = System.Drawing.Color.DimGray;
+            this.btnMoons.Font = new System.Drawing.Font("Bernard MT Condensed", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoons.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnMoons.Location = new System.Drawing.Point(132, 381);
+            this.btnMoons.Name = "btnMoons";
+            this.btnMoons.Size = new System.Drawing.Size(197, 87);
+            this.btnMoons.TabIndex = 2;
+            this.btnMoons.Text = "Moons";
+            this.btnMoons.UseVisualStyleBackColor = false;
+            // 
+            // btnSky
+            // 
+            this.btnSky.BackColor = System.Drawing.Color.DimGray;
+            this.btnSky.Font = new System.Drawing.Font("Bernard MT Condensed", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSky.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSky.Location = new System.Drawing.Point(398, 268);
+            this.btnSky.Name = "btnSky";
+            this.btnSky.Size = new System.Drawing.Size(197, 87);
+            this.btnSky.TabIndex = 3;
+            this.btnSky.Text = "Night Sky";
+            this.btnSky.UseVisualStyleBackColor = false;
+            this.btnSky.Click += new System.EventHandler(this.btnSky_Click);
+            // 
+            // btnGalaxies
+            // 
+            this.btnGalaxies.BackColor = System.Drawing.Color.DimGray;
+            this.btnGalaxies.Font = new System.Drawing.Font("Bernard MT Condensed", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGalaxies.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGalaxies.Location = new System.Drawing.Point(132, 497);
+            this.btnGalaxies.Name = "btnGalaxies";
+            this.btnGalaxies.Size = new System.Drawing.Size(197, 87);
+            this.btnGalaxies.TabIndex = 4;
+            this.btnGalaxies.Text = "Galaxies";
+            this.btnGalaxies.UseVisualStyleBackColor = false;
+            this.btnGalaxies.Click += new System.EventHandler(this.btnGalaxies_Click);
+            // 
+            // btnEarth
+            // 
+            this.btnEarth.BackColor = System.Drawing.Color.DimGray;
+            this.btnEarth.Font = new System.Drawing.Font("Bernard MT Condensed", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEarth.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEarth.Location = new System.Drawing.Point(398, 154);
+            this.btnEarth.Name = "btnEarth";
+            this.btnEarth.Size = new System.Drawing.Size(197, 87);
+            this.btnEarth.TabIndex = 5;
+            this.btnEarth.Text = "Earth";
+            this.btnEarth.UseVisualStyleBackColor = false;
+            this.btnEarth.Click += new System.EventHandler(this.btnEarth_Click);
+            // 
+            // btnPlanets
+            // 
+            this.btnPlanets.BackColor = System.Drawing.Color.DimGray;
+            this.btnPlanets.Font = new System.Drawing.Font("Bernard MT Condensed", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlanets.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnPlanets.Location = new System.Drawing.Point(132, 268);
+            this.btnPlanets.Name = "btnPlanets";
+            this.btnPlanets.Size = new System.Drawing.Size(197, 87);
+            this.btnPlanets.TabIndex = 6;
+            this.btnPlanets.Text = "Planets";
+            this.btnPlanets.UseVisualStyleBackColor = false;
+            // 
+            // btnStars
+            // 
+            this.btnStars.BackColor = System.Drawing.Color.DimGray;
+            this.btnStars.Font = new System.Drawing.Font("Bernard MT Condensed", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStars.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnStars.Location = new System.Drawing.Point(398, 381);
+            this.btnStars.Name = "btnStars";
+            this.btnStars.Size = new System.Drawing.Size(197, 87);
+            this.btnStars.TabIndex = 7;
+            this.btnStars.Text = "Stars";
+            this.btnStars.UseVisualStyleBackColor = false;
+            // 
+            // btnNebulae
+            // 
+            this.btnNebulae.BackColor = System.Drawing.Color.DimGray;
+            this.btnNebulae.Font = new System.Drawing.Font("Bernard MT Condensed", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNebulae.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNebulae.Location = new System.Drawing.Point(398, 497);
+            this.btnNebulae.Name = "btnNebulae";
+            this.btnNebulae.Size = new System.Drawing.Size(197, 87);
+            this.btnNebulae.TabIndex = 8;
+            this.btnNebulae.Text = "Nebulae";
+            this.btnNebulae.UseVisualStyleBackColor = false;
+            // 
+            // txtboxCart
+            // 
+            this.txtboxCart.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtboxCart.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxCart.Location = new System.Drawing.Point(710, 141);
+            this.txtboxCart.Multiline = true;
+            this.txtboxCart.Name = "txtboxCart";
+            this.txtboxCart.ReadOnly = true;
+            this.txtboxCart.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtboxCart.Size = new System.Drawing.Size(380, 341);
+            this.txtboxCart.TabIndex = 9;
+            this.txtboxCart.Text = resources.GetString("txtboxCart.Text");
+            this.txtboxCart.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.DimGray;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmit.Font = new System.Drawing.Font("Bernard MT Condensed", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.SpringGreen;
+            this.btnSubmit.Location = new System.Drawing.Point(814, 503);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(186, 75);
+            this.btnSubmit.TabIndex = 10;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            // 
+            // lblCart
+            // 
+            this.lblCart.AutoSize = true;
+            this.lblCart.Font = new System.Drawing.Font("Bernard MT Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCart.ForeColor = System.Drawing.SystemColors.Menu;
+            this.lblCart.Location = new System.Drawing.Point(788, 84);
+            this.lblCart.Name = "lblCart";
+            this.lblCart.Size = new System.Drawing.Size(212, 43);
+            this.lblCart.TabIndex = 11;
+            this.lblCart.Text = "Shopping Cart";
+            // 
+            // HomeScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1178, 644);
+            this.Controls.Add(this.lblCart);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtboxCart);
+            this.Controls.Add(this.btnNebulae);
+            this.Controls.Add(this.btnStars);
+            this.Controls.Add(this.btnPlanets);
+            this.Controls.Add(this.btnEarth);
+            this.Controls.Add(this.btnGalaxies);
+            this.Controls.Add(this.btnSky);
+            this.Controls.Add(this.btnMoons);
+            this.Controls.Add(this.lblDigi);
+            this.Controls.Add(this.btnLights);
+            this.Name = "HomeScreen";
+            this.Text = "DigiScriptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLights;
+        private System.Windows.Forms.Label lblDigi;
+        private System.Windows.Forms.Button btnMoons;
+        private System.Windows.Forms.Button btnSky;
+        private System.Windows.Forms.Button btnGalaxies;
+        private System.Windows.Forms.Button btnEarth;
+        private System.Windows.Forms.Button btnPlanets;
+        private System.Windows.Forms.Button btnStars;
+        private System.Windows.Forms.Button btnNebulae;
+        private System.Windows.Forms.TextBox txtboxCart;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblCart;
+    }
+}
+
---
+++ DigiScriptor/DigiScriptor/HomeScreen.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigiScriptor
+{
+    public partial class HomeScreen : Form
+    {
+        public static HomeScreen Current;
+        public HomeScreen()
+        {
+            InitializeComponent();
+
+            //used to keep track of this form
+            Current = this;
+        }
+
+        private void btnLights_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEarth_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSky_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGalaxies_Click(object sender, EventArgs e)
+        {
+            //hiding the home screen
+            this.Hide();
+
+            //opening up the galaxies screen
+            GalaxiesScreen galScreen = new GalaxiesScreen();
+            galScreen.Show();
+        }
+    }
+}
