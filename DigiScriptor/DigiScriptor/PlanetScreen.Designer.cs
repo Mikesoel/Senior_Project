4709b68e8367ca641f6c97ce99ea39ffbfead15d
Julissa Bonilla <julissabonilla@csus.edu>
2020-09-18 02:22:40 -0700

    Add files via upload

---
+++ DigiScriptor/DigiScriptor/PlanetScreen.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace DigiScriptor
+{
+    partial class PlametScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMercury = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.btnSubmitGalaxy = new System.Windows.Forms.Button();
+            this.lblPlanets = new System.Windows.Forms.Label();
+            this.btnGalaxiesBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMercury
+            // 
+            this.btnMercury.BackColor = System.Drawing.Color.DimGray;
+            this.btnMercury.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnMercury.Location = new System.Drawing.Point(21, 139);
+            this.btnMercury.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMercury.Name = "btnMercury";
+            this.btnMercury.Size = new System.Drawing.Size(147, 41);
+            this.btnMercury.TabIndex = 1;
+            this.btnMercury.Text = "Mercury";
+            this.btnMercury.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(21, 193);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Venus\r\n";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(21, 250);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Earth";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DimGray;
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(21, 304);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Mars";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.DimGray;
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(189, 139);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(147, 41);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Jupiter";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DimGray;
+            this.button6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.Location = new System.Drawing.Point(189, 193);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(147, 41);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Saturn";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.DimGray;
+            this.button7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.Location = new System.Drawing.Point(189, 250);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(147, 41);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Uranus";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.DimGray;
+            this.button8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button8.Location = new System.Drawing.Point(189, 304);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(147, 41);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Neptune";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // btnSubmitGalaxy
+            // 
+            this.btnSubmitGalaxy.BackColor = System.Drawing.Color.DimGray;
+            this.btnSubmitGalaxy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmitGalaxy.Font = new System.Drawing.Font("Bernard MT Condensed", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmitGalaxy.ForeColor = System.Drawing.Color.SpringGreen;
+            this.btnSubmitGalaxy.Location = new System.Drawing.Point(171, 426);
+            this.btnSubmitGalaxy.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSubmitGalaxy.Name = "btnSubmitGalaxy";
+            this.btnSubmitGalaxy.Size = new System.Drawing.Size(165, 60);
+            this.btnSubmitGalaxy.TabIndex = 17;
+            this.btnSubmitGalaxy.Text = "Submit";
+            this.btnSubmitGalaxy.UseVisualStyleBackColor = false;
+            // 
+            // lblPlanets
+            // 
+            this.lblPlanets.AutoSize = true;
+            this.lblPlanets.Font = new System.Drawing.Font("Bernard MT Condensed", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlanets.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lblPlanets.Location = new System.Drawing.Point(26, 33);
+            this.lblPlanets.Name = "lblPlanets";
+            this.lblPlanets.Size = new System.Drawing.Size(157, 55);
+            this.lblPlanets.TabIndex = 18;
+            this.lblPlanets.Text = "Planets";
+            // 
+            // btnGalaxiesBack
+            // 
+            this.btnGalaxiesBack.BackColor = System.Drawing.Color.Gray;
+            this.btnGalaxiesBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGalaxiesBack.Location = new System.Drawing.Point(21, 455);
+            this.btnGalaxiesBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGalaxiesBack.Name = "btnGalaxiesBack";
+            this.btnGalaxiesBack.Size = new System.Drawing.Size(76, 31);
+            this.btnGalaxiesBack.TabIndex = 19;
+            this.btnGalaxiesBack.Text = "Back";
+            this.btnGalaxiesBack.UseVisualStyleBackColor = false;
+            // 
+            // PlametScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1047, 515);
+            this.Controls.Add(this.btnGalaxiesBack);
+            this.Controls.Add(this.lblPlanets);
+            this.Controls.Add(this.btnSubmitGalaxy);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnMercury);
+            this.Name = "PlametScreen";
+            this.Text = "Planets";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private System.Windows.Forms.Button btnMercury;
+        #endregion
+
+        private System.Windows.Forms.Button btnVenus;
+        private System.Windows.Forms.Button btnEarth;
+        private System.Windows.Forms.Button btnMars;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btnSubmitGalaxy;
+        private System.Windows.Forms.Label lblPlanets;
+        private System.Windows.Forms.Button btnGalaxiesBack;
+    }
+}
+
---
+++ DigiScriptor/DigiScriptor/PlanetScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DigiScriptor
+{
+    public partial class PlametScreen : Form
+    {
+        public static PlametScreen Current;
+        public PlametScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void PlametScreen_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //open earth page
+        }
+
+       
+    }
+}
