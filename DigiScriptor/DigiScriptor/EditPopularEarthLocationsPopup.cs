716d2fd70e4d6bc5038f89584343d9e9ba117783
Richard Schorling <rschorling@csus.edu>
2020-10-01 09:43:33 -0700

    Added DB functionality to earth screen combobox and add/edit functionality

---
+++ DigiScriptor/DigiScriptor/EditPopularEarthLocationsPopup.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DigiScriptor
+{
+    public partial class EditPopularEarthLocationsPopup : Form
+    {
+
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Richie\Source\Repos\Mikesoel\Senior_Project\DigiScriptor\DigiScriptor\DigiDataBase.mdf;Integrated Security=True");
+        private string name = string.Empty;
+        private int latitude, longitude;
+
+        public EditPopularEarthLocationsPopup()
+        {
+            InitializeComponent();
+            LoadTable();
+
+        }
+
+        private void LoadTable()
+
+        {
+            connect.Open();
+            SqlCommand cmd = connect.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select Name, Latitude, Longitude from EarthScreenFavorites";
+            cmd.ExecuteNonQuery();
+
+            try
+            {
+                SqlDataAdapter sda = new SqlDataAdapter();
+                sda.SelectCommand = cmd;
+                DataTable dbdataset = new DataTable();
+                sda.Fill(dbdataset);
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dbdataset;
+                dataGridView1.DataSource = bSource;
+                sda.Update(dbdataset);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            connect.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataSave_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+            SqlCommand cmd = connect.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into EarthScreenFavorites (Name, Latitude, Longitude, LocationID) VALUES (@Name, @Latitude, @Longitude, (SELECT (MAX(LocationID) + 1) from EarthScreenFavorites))";
+            cmd.Parameters.AddWithValue("@Name", name);
+            cmd.Parameters.AddWithValue("@Latitude", latitude);
+            cmd.Parameters.AddWithValue("@Longitude", longitude);
+            cmd.ExecuteNonQuery();
+            connect.Close();
+            LoadTable();
+
+        }
+
+        private void latitudeTextBox_TextChanged(object sender, EventArgs e)
+        {
+            latitude = Int32.Parse(latitudeTextBox.Text);
+        }
+
+        private void longitudeTextBox_TextChanged(object sender, EventArgs e)
+        {
+            longitude = Int32.Parse(longitudeTextBox.Text);
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+            SqlCommand cmd = connect.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                cmd.CommandText = "DELETE FROM EarthScreenFavorites WHERE LocationID = @index";
+                cmd.Parameters.AddWithValue("@index", row.Index+1);
+                cmd.ExecuteNonQuery();
+            }
+            connect.Close();
+            LoadTable();
+        }
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            name = nameTextBox.Text;
+        }
+
+
+    }
+}
--- DigiScriptor/DigiScriptor/UserControlEarth.cs
+++ DigiScriptor/DigiScriptor/UserControlEarth.cs
@@ -7,22 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DigiScriptor
 {
     public partial class UserControlEarth : UserControl
     {
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Richie\Source\Repos\Mikesoel\Senior_Project\DigiScriptor\DigiScriptor\DigiDataBase.mdf;Integrated Security=True");
+
+
         public UserControlEarth()
         {
             InitializeComponent();
+            LoadComboBox();
+
+        }
+
+        public void LoadComboBox()
+        {
+            popularLocationsCombo.Items.Clear();
+            connect.Open();
+            SqlCommand cmd = connect.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select Name from EarthScreenFavorites";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                popularLocationsCombo.Items.Add(dr["Name"].ToString());
+            }
+            connect.Close();
         }
 
         private void popularLocationsCombo_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (popularLocationsCombo.Text == "Add New")
-            {
-                MessageBox.Show("Hey");
-            }
+
+        }
+
+        private void popularLocationsCombo_Click(object sender, EventArgs e)
+        {
+            LoadComboBox();
+        }
+
+
+        private void editPopularLocationsButton_Click(object sender, EventArgs e)
+        {
+            EditPopularEarthLocationsPopup editData = new EditPopularEarthLocationsPopup();
+            editData.Show();
+
         }
     }
 }
