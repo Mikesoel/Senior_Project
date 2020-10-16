c6447163a3d0f223c166bf6ee60af55c71b2aa07
michael leos <mleos@csus.edu>
2020-10-16 04:18:00 -0700

    Validate favorite star database input by Michael

--- DigiScriptor/DigiScriptor/EditPopularStarsPopup.cs
+++ DigiScriptor/DigiScriptor/EditPopularStarsPopup.cs
@@ -23,6 +23,19 @@ namespace DigiScriptor
         private string cellName = string.Empty;
 
 
+        //Booleans to check data
+        private Boolean DecDTxt_Valid = false;
+        private Boolean DecMinTxt_Valid = false;
+        private Boolean DecSecTxt_Valid = false;
+        private Boolean RAHrTxt_Valid = false;
+        private Boolean RAMinTxt_Valid = false;
+        private Boolean RASecTxt_Valid = false;
+        private Boolean Name_Valid = false;
+
+
+
+
+
         public EditPopularStarsPopup()
         {
             InitializeComponent();
@@ -98,6 +111,321 @@ namespace DigiScriptor
 
         private void btnStarSave_Click(object sender, EventArgs e)
         {
+
+            if (RAHrTxt_Valid ==false || RAMinTxt_Valid ==false || RASecTxt_Valid ==false)
+            {
+                MessageBox.Show("Right Ascension not correct");
+                RAHrTxt.Focus();
+            }
+
+            if (DecDTxt_Valid == false || DecMinTxt_Valid == false || DecSecTxt_Valid == false)
+            {
+                MessageBox.Show("Declination not correct");
+                DecDTxt.Focus();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void EditPopularStarsPopup_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void RAsHrTxt_TextChanged(object sender, EventArgs e)
+        {
+
+            //check if text is empty
+            if (RAHrTxt.Text != "")
+            {
+                int value = 0;
+
+                //if something is in box try to convert to int
+                try
+                {
+                    value = Convert.ToInt32(RAHrTxt.Text);
+                    //validate data is within correct range
+                    if (value >= 0 && value <= 23)
+                    {
+                        //if correct keep text black
+                        RAHrTxt.ForeColor = Color.Black;
+                        RAHrTxt_Valid = true;
+
+                    }
+                    else
+                    {
+                        //if invalid value then change to red text
+                        RAHrTxt.ForeColor = Color.Red;
+                        RAHrTxt_Valid = false;
+                    }
+                }
+                catch
+                {
+                    //if not a number then change text to red
+                    RAHrTxt.ForeColor = Color.Red;
+                    RAHrTxt_Valid = false;
+                }
+
+            }
+            else
+            {
+                //no data input
+                RAHrTxt_Valid = false;
+            }
+
+        }
+
+        private void RAsMinTxt_TextChanged(object sender, EventArgs e)
+        {
+
+            //check if text is empty
+            if (RAsMinTxt.Text != "")
+            {
+                int value = 0;
+
+                //if something is in box try to convert to int
+                try
+                {
+                    value = Convert.ToInt32(RAsMinTxt.Text);
+                    //validate data is within correct range
+                    if (value >= 0 && value <= 59)
+                    {
+                        //if correct keep text black
+                        RAsMinTxt.ForeColor = Color.Black;
+                        RAMinTxt_Valid = true;
+
+                    }
+                    else
+                    {
+                        //if invalid value then change to red text
+                        RAsMinTxt.ForeColor = Color.Red;
+                        RAMinTxt_Valid = false;
+                    }
+                }
+                catch
+                {
+                    //if not a number then change text to red
+                    RAsMinTxt.ForeColor = Color.Red;
+                    RAMinTxt_Valid = false;
+                }
+
+            }
+            else
+            {
+                //no data input
+                RAMinTxt_Valid = false;
+            }
+
+        }
+        
+
+        private void RAsSecTxt_TextChanged(object sender, EventArgs e)
+        {
+            //check if text is empty
+            if (RAsSecTxt.Text != "")
+            {
+                int value = 0;
+
+                //if something is in box try to convert to int
+                try
+                {
+                    value = Convert.ToInt32(RAsSecTxt.Text);
+                    //validate data is within correct range
+                    if (value >= 0 && value <= 59)
+                    {
+                        //if correct keep text black
+                        RAsSecTxt.ForeColor = Color.Black;
+                        RASecTxt_Valid = true;
+
+                    }
+                    else
+                    {
+                        //if invalid value then change to red text
+                        RAsSecTxt.ForeColor = Color.Red;
+                        RASecTxt_Valid = false;
+                    }
+                }
+                catch
+                {
+                    //if not a number then change text to red
+                    RAsSecTxt.ForeColor = Color.Red;
+                    RASecTxt_Valid = false;
+                }
+
+            }
+            else
+            {
+                //no data input
+                RASecTxt_Valid = false;
+            }
+
+
+
+        }
+
+        private void DecDTxt_TextChanged(object sender, EventArgs e)
+        {
+
+            //check if text is empty
+            if (DecDTxt.Text != "")
+            {
+                int value = 0;
+
+                //if something is in box try to convert to int
+                try
+                {
+                    value = Convert.ToInt32(DecDTxt.Text);
+                    //validate data is within correct range
+                    if (value >= -90 && value <= 90)
+                    {
+                        //if correct keep text black
+                        DecDTxt.ForeColor = Color.Black;
+                        //data is valid
+                        DecDTxt_Valid = true;
+                    }
+                    else
+                    {
+                        //if invalid value then change to red text
+                        DecDTxt.ForeColor = Color.Red;
+                        //data is invalid
+                        DecDTxt_Valid = false;
+                    }
+                }
+                catch
+                {
+                    //if not a number then change text to red
+                    DecDTxt.ForeColor = Color.Red;
+                    //data is invalid
+                    DecDTxt_Valid = false;
+                }
+
+            }
+            else
+            {
+                //no data input
+                DecDTxt_Valid = false;
+            }
+
+
+
+
+        }
+
+        private void DecMinTxt_TextChanged(object sender, EventArgs e)
+        {
+            //check if text is empty
+            if (DecMinTxt.Text != "")
+            {
+                int value = 0;
+
+                //if something is in box try to convert to int
+                try
+                {
+                    value = Convert.ToInt32(DecMinTxt.Text);
+                    //validate data is within correct range
+                    if (value >= 0 && value <= 59)
+                    {
+                        //if correct keep text black
+                        DecMinTxt.ForeColor = Color.Black;
+                        DecMinTxt_Valid = true;
+                    }
+                    else
+                    {
+                        //if invalid value then change to red text
+                        DecMinTxt.ForeColor = Color.Red;
+                        DecMinTxt_Valid = false;
+                    }
+                }
+                catch
+                {
+                    //if not a number then change text to red
+                    DecMinTxt.ForeColor = Color.Red;
+                    DecMinTxt_Valid = false;
+                }
+
+            }
+            else
+            {
+                //no data input
+                DecMinTxt_Valid = false;
+            }
+
+        }
+
+        private void DecSecTxt_TextChanged(object sender, EventArgs e)
+        {
+
+            //check if text is empty
+            if (DecSecTxt.Text != "")
+            {
+                int value = 0;
+
+                //if something is in box try to convert to int
+                try
+                {
+                    value = Convert.ToInt32(DecSecTxt.Text);
+                    //validate data is within correct range
+                    if (value >= 0 && value <= 59)
+                    {
+                        //if correct keep text black
+                        DecSecTxt.ForeColor = Color.Black;
+                        DecSecTxt_Valid = true;
+
+
+                    }
+                    else
+                    {
+                        //if invalid value then change to red text
+                        DecSecTxt.ForeColor = Color.Red;
+                        DecSecTxt_Valid = false;
+                    }
+                }
+                catch
+                {
+                    //if not a number then change text to red
+                    DecSecTxt.ForeColor = Color.Red;
+                    DecSecTxt_Valid = false;
+                }
+
+            }
+            else
+            {
+                //no data input
+                DecSecTxt_Valid = false;
+            }
+
+        }
+
+        private void NametextBox_TextChanged(object sender, EventArgs e)
+        {
+
+            if(NametextBox.Text == "" || NametextBox.Text.StartsWith(" "))
+            {
+                Name_Valid = false;
+            }
 
         }
     }
